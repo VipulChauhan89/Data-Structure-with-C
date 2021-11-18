76955225338ebbb7cd8b78ffbe7706cd7444855c
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2021-11-18 07:40:33 +0530

    Update TOH.c

--- Tower-of-Hanoi/TOH.c
+++ Tower-of-Hanoi/TOH.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<math.h>
-void toh(int n,char source,char auxilary,char destination)
+void toh(int n,char a,char b,char c)
 {
     if(n==1)
     {
-        printf("Move disk %d from %c to %c\n",n,source,destination);
+        printf("Move disk %d from tower %c to tower %c\n",n,a,b);
         return ;
     }
-    toh(n-1,source,destination,auxilary);
-    printf("Move disk %d from %c to %c\n",n,source,destination);
-    toh(n-1,destination,auxilary,source); 
+    toh(n-1,a,c,b);
+    printf("Move disk %d from tower %c to tower %c\n",n,a,b);
+    toh(n-1,c,b,a); 
 }
 int main()
 {
@@ -19,6 +19,6 @@ int main()
     steps=pow(2,n)-1;
     printf("Total steps to move all the disks from source to destination is = %d\n",steps);
     printf("The steps are : \n");
-    toh(n,'S','A','D');
+    toh(n,'A','B','C');
     return 0;
 }
